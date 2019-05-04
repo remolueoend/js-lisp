51659fdbdb24c0f58c178178d7eb4f1e2a88cf0d
Remo Zumsteg <remolueoend@users.noreply.github.com>
2019-05-04 18:49:59 +0200

    feat(index) add npm start script

--- index.js
+++ index.js
@@ -6,16 +6,4 @@ const { test, execTests, assert } = require("./test")
 
 let exp = [def, [1, 2, 3], 8, (data, base) => [map, [add, base], data]]
 
-let testAdd = [
-  test,
-  "adds two numbers correctly",
-  [assert.isEqual, 3, [add, 1, 2]],
-]
-
-let testAddCurried = [
-  test,
-  "can be curried correctly",
-  [assert.isEqual, 3, [[add, 1], 2]],
-]
-
-execTests([testAdd, testAddCurried])
+console.log(evalExp(exp))
